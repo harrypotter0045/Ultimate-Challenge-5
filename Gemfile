37ef95b0209d2564b3e604a3d1d3357862b68f21
harrypotter0045 <harrypotter0045@gmail.com>
2020-10-05 02:40:04 -0500

    Create the seed script and populate the products

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.7.1'
 
+# Add the Faker gem
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.3'
 # Use sqlite3 as the database for Active Record
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.delete_all
+
+676.times do
+  product = Product.create(
+    title:          Faker::Commerce.product_name,
+    price:          Faker::Commerce.price,
+    stock_quantity: Faker::Number.number(digits: 3)
+  )
+end
