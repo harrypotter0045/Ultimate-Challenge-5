eec2446c7c469de51fecb196ae7ece465a32c2fb
harrypotter0045 <harrypotter0045@gmail.com>
2020-10-05 10:34:36 -0500

    Add reference between categories and products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_05_071805) do
+ActiveRecord::Schema.define(version: 2020_10_05_152931) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
@@ -19,6 +25,9 @@ ActiveRecord::Schema.define(version: 2020_10_05_071805) do
     t.integer "stock_quantity"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
+  add_foreign_key "products", "categories"
 end
