72b2abd71bb9b422b0431171ddb401da3c0811fd
harrypotter0045 <harrypotter0045@gmail.com>
2020-10-05 11:59:04 -0500

    Challenge complete commit

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ApplicationRecord
-  belongs_to :categories
+  belongs_to :category
   validates :title, :price, :stock_quantity, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
 
-Product.delete_all
+Product.destroy_all
 
-676.times do
-  product = Product.create(
-    title:          Faker::Commerce.product_name,
-    price:          Faker::Commerce.price,
-    stock_quantity: Faker::Number.number(digits: 3)
-  )
+Category.destroy_all
+
+csv_file = Rails.root.join("db/products.csv")
+csv_data = File.read(csv_file)
+
+products = CSV.parse(csv_data, headers: true)
+
+products.each do |p|
+  category = Category.find_or_create_by(name: p["category"])
+  if category&.valid?
+    product = category.products.create(
+      title:          p["name"],
+      price:          p["price"],
+      stock_quantity: p["stock quantity"],
+      description:    p["description"]
+    )
+
+    puts product.inspect unless product&.valid?
+    puts category.inspect unless product&.valid?
+    puts "Invald product #{p['name']}" unless product&.valid?
+    puts product.errors.messages.inspect unless product&.valid?
+  else
+    puts "Invalid category #{p['category']} for product #{p['name']}."
+  end
 end
+
+puts "Created #{Category.count}"
+puts "Created #{Product.count}"
