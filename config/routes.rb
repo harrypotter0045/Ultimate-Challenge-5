06bcfb61a3f77d23d90a38da316f7d16434dccb1
harrypotter0045 <harrypotter0045@gmail.com>
2020-10-05 03:33:15 -0500

    Add the show view and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # Routes for All Products
   get "/products", to: "products#index", as: "products"
+
+  # Routes for Individual Product
+  get "/products/:id", to: "products#show", as: "product"
 end
